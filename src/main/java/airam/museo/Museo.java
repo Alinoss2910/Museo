e816d973f2a2b02bd7a4a1043a7ceb922318d92a
alinoss2910 <alinoss2910@g.educaand.es>
2023-10-04 19:53:22 +0200

    Terminado

--- src/main/java/airam/museo/Museo.java
+++ src/main/java/airam/museo/Museo.java
@@ -5,7 +5,6 @@
 package airam.museo;
 
 import java.time.LocalDate;
-import java.util.List;
 
 /**
  *
@@ -13,45 +12,37 @@ import java.util.List;
  */
 public class Museo {
     
-    public Catalogo catalogo = new Catalogo();
+    public static Catalogo catalogo = new Catalogo();
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         
         Artista artista1 = new Artista("Airam", "Jerez", LocalDate.MIN, LocalDate.MAX);
         Artista artista2 = new Artista("Alvaro", "Jerez", LocalDate.MIN, LocalDate.MAX);
         Artista artista3 = new Artista("Juan", "Jerez", LocalDate.MIN, LocalDate.MAX);
         
-        Pinturas pintura1 = new Pinturas("2x2", "Lienzo", "Blanca Navidad", 1, artista1, LocalDate.MIN);
-        Pinturas pintura2 = new Pinturas("3x2", "Lienzo", "Azur", 2, artista2, LocalDate.MIN);
-        Pinturas pintura3 = new Pinturas("4x2", "Lienzo", "Kyros", 3, artista3, LocalDate.MIN);
+        Pinturas pintura1 = new Pinturas(2, "Lienzo", "Blanca Navidad", 1, artista1, LocalDate.MIN);
+        Pinturas pintura2 = new Pinturas(3, "Lienzo", "Azur", 2, artista2, LocalDate.MIN);
+        Pinturas pintura3 = new Pinturas(4, "Lienzo", "Kyros", 3, artista3, LocalDate.MIN);
         
         Esculturas escultura1 = new Esculturas("Piedra", 2.18, "Pensador", 4, artista1, LocalDate.MIN);
         Esculturas escultura2 = new Esculturas("Marmol", 3.18, "Boleador", 5, artista1, LocalDate.MIN);
         Esculturas escultura3 = new Esculturas("Caliza", 4.18, "Cansado", 6, artista1, LocalDate.MIN);
         
-        añadeObra(pintura3);
+        catalogo.añadeObra(pintura1);
+        catalogo.añadeObra(pintura2);
+        catalogo.añadeObra(pintura3);
+        catalogo.eliminarObra(3);
+        catalogo.añadeObra(escultura1);
+        catalogo.añadeObra(escultura2);
+        catalogo.añadeObra(escultura3);
+        catalogo.añadeObra(escultura3);
+        catalogo.buscarObra(6);
+        
+        catalogo.masAlta();
+        catalogo.superficie();
     }
     
     
     
-    public void añadeObra(ObraArtistica ob) {
-        
-        catalogo.setObra(ob);
-        System.out.println(catalogo);
-        
-    }
     
-    public void eliminarObra(int nI) {
-        List cat = catalogo.getObra();
-        for(int i = 0; i <= cat.size(); i++) {
-            for(int j = 0; j <= cat(i);j++) {
-                
-            }
-        }
-        
-    }
-    
-    public void buscarObra(int nI) {
-        
-    }
 }
